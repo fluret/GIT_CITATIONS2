ed8073f4d40c44dbcc2bb336143940e6082a61e7
Frédéric LURET <frederic.luret@univ-tours.fr>
2026-09-24 11:28:50 +0200

    Affichage aléatoire des citations

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from random import choice
+
 from pyfiglet import figlet_format
 from rich import print
 
+with open("citations.txt", "r", encoding="utf-8") as f:
+    citations = f.readlines()
+
+citations = [c.strip() for c in citations]
+citation = choice(citations)
+
 print(figlet_format("Citation"))
-print("[cyan]La meilleure façon de prédire l'avenir est de l'inventer.[/cyan]")
+print(f"[cyan]{citation}[/cyan]")
